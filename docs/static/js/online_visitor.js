b4d86b8acb4e2159261f6640f1935e54211da674
llody <745719408@qq.com>
2024-09-18 19:33:42 +0800

    统计在线人数

--- docs/static/js/online_visitor.js
+++
@@ -1,5 +0,0 @@
-window.onload = function(){
-    new Oneline("using-a-z-and-0-9-to-make-a-unique-id-for-your-website").on("OnelineUpdate",(e)=>{
-    document.querySelector("#visitor-count").innerText = e.detail.count;
-  })
-  }
